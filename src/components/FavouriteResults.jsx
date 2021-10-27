316152f1fa4ef3508a09a35a2351e24fc36dd752
Rahel Yohannes Woticha <83081065+rita342@users.noreply.github.com>
2021-10-27 06:05:37 +0100

    commit

--- src/components/FavouriteResults.jsx
+++ src/components/FavouriteResults.jsx
@@ -2,25 +2,30 @@ import React from 'react'
 import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { addToFav, removeFromFav } from '../store/actions'
-import { connect } from 'react-redux'
+//import { connect } from 'react-redux'
+import { useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 
-const mapStateToProps = s => s
+//const mapStateToProps = s => s
 
-const mapDispatchToProps = (dispatch) => ({
-    addToFavourites: (company) => dispatch(addToFav(company)),
-    removeFromFavourites: (company) => dispatch(removeFromFav(company))
-})
+//const mapDispatchToProps = (dispatch) => ({
+  //  addToFavourites: (company) => dispatch(addToFav(company)),
+    //removeFromFavourites: (company) => dispatch(removeFromFav(company))
+//})
 
 
-function FavouriteResults({ data, favourites, addToFavourites, removeFromFavourites }) {
+//function FavouriteResults({ data, favourites, addToFavourites, removeFromFavourites }) {
+function  FavouriteResults({data}) { 
+const favourites = useSelector((state) => state.favourites)
+const dispatch = useDispatch();
 
     const isFav = favourites.includes(data.company_name)
     console.log(isFav, favourites)
     const toggleFavourite = () => {
         isFav 
-            ? removeFromFavourites(data.company_name) 
-            : addToFavourites(data.company_name) 
+            ? dispatch(removeFromFav(data.company_name) )
+            : dispatch(addToFav(data.company_name) )
     }
 
     return (
@@ -31,7 +36,7 @@ function FavouriteResults({ data, favourites, addToFavourites, removeFromFavouri
                         ? <button className="btn btn-danger" size={16} className="me-4 my-auto" onClick={toggleFavourite}>add to favourite</button>
                         : <button color="gold" size={16} className="me-4 my-auto" onClick={toggleFavourite}>its added</button>
                 }
-                <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+                <Link to={`/${data.title}`}>{data.title}</Link>
             </Col>
             
         </Row>
@@ -39,4 +44,5 @@ function FavouriteResults({ data, favourites, addToFavourites, removeFromFavouri
 }
 
 
-export default connect(mapStateToProps, mapDispatchToProps)(FavouriteResults)
+//export default connect(mapStateToProps, mapDispatchToProps)(FavouriteResults)
+export default FavouriteResults
